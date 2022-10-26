f4e1b98ced96f47ad8adb392952ba9c04d7a4688
Daniel Sagastume <daniesbw@unitec.edu>
2022-10-25 21:55:54 -0600

    luis se la come

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login'
+import Login from './Login';
+import Mod2 from "./Mod2";
+
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-
-
-=======
-    <Login/>
->>>>>>> 727a29a3a1093805af7b60fc31138ff401b6a2b5
+    <Mod2/>
+    
     </div>
   );
 }
---
+++ my-app/src/Mod2.js
@@ -0,0 +1,7 @@
+function Mod2() {
+    return (
+      <div>mod2</div>
+    )
+  }
+  
+  export default Mod2
